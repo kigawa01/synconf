c9c578429a35af197398cf7736cdf890bf5877bb
kigawa <kigawa.8390@gmail.com>
2022-02-02 18:33:30 +0900

    update

---
+++ setting.bash
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+NAMES=(
+"raid-setting"
+)
+URLS=(
+"https://github.com/kigawa8390/raid-setting.git"
+)
+
+function yn() {
+  if [ $# -eq 1 ]; then
+
+    while [ "$r" = 'y' ] || [ "$r" = 'n' ]; do
+      echo "$1 type y/n"
+      read r
+    done
+    if [ "$r" = 'y' ] || [ "$r" = '' ]; then
+      return 0
+    fi
+    return 1
+  fi
+  exit 1
+}
+
+function var() {
+  while [ $allow -eq 0 ]; do
+    read r
+    for i; do
+      if [ $i = $r ] || [ $i = "*" ]; then
+        allow=0
+        break
+      fi
+    done
+  done
+  echo $r
+}
+
+echo type branch
+BRANCH=$(var *)
+
+for i in {0..$((${#NAMES[@]}-1))};do
+  NAME="${NAMES[i]}"
+  URL="${URLS[i]}"
+  yn "are you setup ${NAME}?"
+  if [ "$?" -eq 0 ]; then
+    if [ ! -d "./${NAME}" ]; then
+      git clone "${URL}"
+    fi
+    (
+    cd "${NAME}"
+    git fetch
+    git merge
+    chmod 700 ./setting.bash
+    ./setting.bash
+    )
+  fi
+done
